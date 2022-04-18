40a4f36716f4ecbeca8acfc835c2e4a29e8973b0
Andres Moral Carballo <andremoralcarballo@gmail.com>
2022-04-18 11:56:47 -0400

    EJECUCION CON EL MAIN

--- ListaSE.java
+++ ListaSE.java
@@ -16,11 +16,15 @@ public class ListaSE {
     public int tamaño(){
         int contador = 0;
         Nodo aux = inicio;
-        while (aux != null){
+        if(esta_vacia()){
+            System.out.println("La lista esta vacia");
+        }else{
+            while (aux != null){
 
-            contador ++;
-            aux = aux.getSiguiente();
-
+                contador ++;
+                aux = aux.getSiguiente();
+    
+            }
         }
 
         return contador;
@@ -30,12 +34,47 @@ public class ListaSE {
     public Object ultimoDato(){
         Nodo aux = inicio;
 
-        while(aux.getSiguiente() != null){
-            aux = aux.getSiguiente();
-
+        if(esta_vacia()){
+            System.out.println("La lista esta vacia");
+        }else{
+            while(aux.getSiguiente() != null){
+                aux = aux.getSiguiente();
+    
+            }
         }
 
         return aux.getSiguiente();
+    }
+
+    //insertar
+    public void insertarInicio(Object dato){
+        if(esta_vacia()){
+            this.inicio = new Nodo(dato, null);
+        }else{
+            Nodo aux = inicio;
+            this.inicio = new Nodo(dato, aux);
+        }
+    }
+
+    //eliminar
+    public void eliminarInicio(){
+        if(!esta_vacia()){
+            this.inicio = inicio.getSiguiente();
+        }
+    }
+
+    public void mostrar(){
+        if(esta_vacia()){
+            System.out.println("La lista esta vacia");
+        }else{
+            Nodo aux = inicio;
+
+            while (aux != null){
+                System.out.println(aux.getDato()+ " ");
+                aux = aux.getSiguiente();
+            }
+            System.out.println();
+        }
     }
 
 }
